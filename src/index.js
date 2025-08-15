773820085a2d825f5ab3d5edd753d69d33580ee4
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2025-08-15 14:11:07 +0300

    added id in html and api in JS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 alert("Welcome to the Poem Generator!");
-
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "At the drive-thru line\nat Taco Bell",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInputElement = document.querySelector("#user-instructions");
+  let apiKey = "103e0470750babf36f99e08ddo7f31ta";
+  let prompt = `Generate a poem about ${instructionsInputElement.value}`;
+  let context = "You are a poet that writes poems based on user instructions. Write a short poem that is creative and engaging.Generate a 4 line poem in basic HTML and follow the user's instructions. The poem should be creative and engaging. Use simple language and structure. Make sure to include a title for the poem.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+  axios.get(apiUrl).then(displayPoem);
+  console.log("generating poem");
+  
 }
 
 let poemformElement = document.querySelector("#poem-generator-form");
