3bd870bf9e5922dcfaf1a002b7b80ce6262cb2f0
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2025-09-20 21:24:24 +0300

    updated index.html

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ function generatePoem(event) {
   let instructionsInputElement = document.querySelector("#user-instructions");
   let apiKey = "103e0470750babf36f99e08ddo7f31ta";
   let prompt = `Generate a poem about ${instructionsInputElement.value}`;
-  let context = "You are a poet that writes poems based on user instructions. Write a short poem that is creative and engaging.Generate a 4 line poem in basic HTML and follow the user's instructions. The poem should be creative and engaging. Use simple language and structure. Make sure to include a title for the poem.";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions.Include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
