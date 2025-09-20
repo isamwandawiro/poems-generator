c1a558e87bb6cf537bcf7b7bf3891ec0088a1c15
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2025-09-20 21:47:16 +0300

    added hints html,css and js

--- src/index.js
+++ src/index.js
@@ -19,10 +19,13 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions.Include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
 
   axios.get(apiUrl).then(displayPoem);
-  console.log("generating poem");
+  
   
 }
 
